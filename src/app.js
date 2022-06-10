d8cbee56a8f79e010a8aa89884ead5381c412f12
Kyle Moses <kmoses@stedwards.edu>
2022-06-10 12:46:33 -0500

    adding changes

--- src/app.js
+++ src/app.js
@@ -52,10 +52,27 @@ app.get('/transfer', (req, res) => {
 
 // Create the Transfer POST Route
 app.post('/transfer', (req, res) => {
-    //
-    return res.status(201)
-    
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10)
+    const accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+    res.render('transfer', {message: 'Transfer Completed'})    
 })
+
+// Create the payment Route
+app.get('/payment', (req, res) => {
+    res.render('payment', {account: accounts.credit})
+})
+
+// Create the payment POST Route
+app.post('/payment', (req, res) => {
+   accounts.credit.balance = accounts.credit.balance - req.body.amount
+   accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount)
+   const accountsJSON = JSON.stringify(accounts, null, 4)
+   fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+   res.render('payment', {message: 'Payment Successful', account: accounts.credit})
+})
+
 
 // Create a Server
 app.listen(3000, () => {
