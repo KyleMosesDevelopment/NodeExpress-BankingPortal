f60ec4496d5e753a24db2b11529754a51177754c
Kyle Moses <kmoses@stedwards.edu>
2022-06-10 13:18:11 -0500

    final module

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const {accounts, users, writeJSON} = require('./data')
+const accountRoutes = require('./routes/accounts')
+const servicesRoutes = require('./routes/services')
 
 const app = express()
 
@@ -20,50 +22,15 @@ app.get('/', (req, res) => {
     res.render('index', {title: 'Account Summary', accounts: accounts})
 })
 
-// Create the Savings Account Route
-app.get('/savings', (req, res) => {
-    res.render('account', {account: accounts.savings})
-})
-
-app.get('/checking', (req, res) => {
-    res.render('account', {account: accounts.checking})
-})
-
-app.get('/credit', (req, res) => {
-    res.render('account', {account: accounts.credit})
-})
-
 // Create the Profile Route
 app.get('/profile', (req, res) => {
     res.render('profile', {user: users[0]})
 })
 
-// Create the Transfer GET Route
-app.get('/transfer', (req, res) => {
-    res.render('transfer', {user: users[0]})
-})
-
-// Create the Transfer POST Route
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10)
-    writeJSON()
-    res.render('transfer', {message: 'Transfer Completed'})    
-})
-
-// Create the payment Route
-app.get('/payment', (req, res) => {
-    res.render('payment', {account: accounts.credit})
-})
-
-// Create the payment POST Route
-app.post('/payment', (req, res) => {
-   accounts.credit.balance = accounts.credit.balance - req.body.amount
-   accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount)
-   writeJSON()
-   res.render('payment', {message: 'Payment Successful', account: accounts.credit})
-})
-
+//ROUTING TO ACCOUNT
+app.use('/account', accountRoutes)
+// ROUTING TO SERVICES
+app.use('/services', servicesRoutes)
 
 // Create a Server
 app.listen(3000, () => {
